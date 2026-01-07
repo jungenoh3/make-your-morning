0018bb9aea4fe0c7a7f355e8353e4de855e99e4f
jungenoh3 <jungeyo3@naver.com>
2026-01-07 16:56:13 +0900

    feat: 서버 추가

---
+++ server/makeyourmorning-server/src/main/java/com/nochunsam/makeyourmorning_server/MakeyourmorningServerApplication.java
@@ -0,0 +1,13 @@
+package com.nochunsam.makeyourmorning_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeyourmorningServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeyourmorningServerApplication.class, args);
+	}
+
+}
---
+++ server/makeyourmorning-server/src/test/java/com/nochunsam/makeyourmorning_server/MakeyourmorningServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nochunsam.makeyourmorning_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeyourmorningServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
