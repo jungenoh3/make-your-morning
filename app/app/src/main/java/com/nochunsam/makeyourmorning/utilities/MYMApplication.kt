2ead77020c8c7e53b26a138ec0d5c634b36ece40
jungenoh3 <jungeyo3@naver.com>
2026-01-08 15:23:53 +0900

    feat: Retrofit 추가 및 API 통신 제작

--- app/app/src/main/java/com/nochunsam/makeyourmorning/utilities/MYMApplication.kt
+++ app/app/src/main/java/com/nochunsam/makeyourmorning/utilities/MYMApplication.kt
@@ -7,10 +7,28 @@ import android.app.NotificationManager
 import android.os.Build
 import com.nochunsam.makeyourmorning.utilities.database.AppDatabase
 import com.nochunsam.makeyourmorning.utilities.database.AppRepository
+import com.nochunsam.makeyourmorning.utilities.retrofit.RetrofitInterface
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import kotlin.getValue
 
 class MYMApplication: Application() {
     private val database by lazy { AppDatabase.getInstance(this)!! }
-    val repository by lazy { AppRepository(database.dayRecordDao()) }
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8080")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private val apiService by lazy {
+        retrofit.create(RetrofitInterface::class.java)
+    }
+
+    val repository by lazy { AppRepository(database.dayRecordDao(), apiService) }
 
     override fun onCreate() {
         super.onCreate()
